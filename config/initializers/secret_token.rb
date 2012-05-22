b853890ca5fa315021c6fcf5e427e0804b357bdb
Omar Vargas <ovargas@nearsoft.com>
2012-05-22 14:09:25 -0700

    First rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Caciquear::Application.config.secret_token = '420a6bd55328d48ccf0c36a05d26ec177314301e5cc1af78387f7852ff8943b30bbe881b01c751f216d8544ea828ce608e49dcff597c8630b1ad13bb60c3e1ea'
