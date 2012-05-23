3f9420ab79cb9eaf548a8f715cbf9cc3f029e6de
Omar Vargas <ovargas@nearsoft.com>
2012-05-22 17:55:59 -0700

    Some changes in how use FG

--- features/step_definitions/book_steps.rb
+++ features/step_definitions/book_steps.rb
@@ -1,13 +1,24 @@
-When /^I fill book form with "(.*?)" book data$/ do |book|
-  book = FactoryGirl.build(:book)
-  fill_in 'Title', :with => book.title 
+When /^I fill book form with "(.*?)" book data$/ do |name|
+  book = book_attributes_for name
+  within "#new_book" do
+    fill_in 'Title', :with => book[:title]
+  end
 end
 
 When /^I submit book form$/ do
   click_on 'Submit'
 end
 
-Then /^"(.*?)" book should been created$/ do |book|
-  book = FactoryGirl.attributes_for(:book)
+Then /^"(.*?)" book should been created$/ do |name|
+  book = book_attributes_for name
   Book.where(title: book[:title]).count.should == 1
+end
+
+def book_attributes_for(title=nil)
+  case title
+  when 'Foo'
+    FactoryGirl.attributes_for(:foo_book)
+  else
+    FactoryGirl.attributes_for(:book, title: title)
+  end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :book do
+    sequence(:title) {|n| "Book-#{n}" }
+  end
+  factory :foo_book, parent: :book do
     title "Foo Book"
   end
 end
